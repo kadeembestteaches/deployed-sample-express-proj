a0bb9131c5009f324d5edfa84432712b47fdea20
kadeembestteaches <56241232+kadeembestteaches@users.noreply.github.com>
2020-06-23 12:20:21 -0400

    Added stuff for heroku to work

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ app.set('view engine', 'handlebars');
 
 app.get("/",(req,res)=>{
 
-    res.render("home",{
+    res.render("HOME",{
         title:"Home Page"
     })
 })
 
 
-const PORT  = 3000;
+const PORT  = process.env.PORT ||  3000;
 app.listen(3000, ()=>{
     console.log("Web Site is up and running");
 })
