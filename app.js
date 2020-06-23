c67b4c7ad7561ee80b46cd4e45c1fe6f46e17d0e
kadeembestteaches <56241232+kadeembestteaches@users.noreply.github.com>
2020-06-23 12:02:15 -0400

    Sample Project Completed

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const exphbs  = require('express-handlebars');
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get("/",(req,res)=>{
+
+    res.render("home",{
+        title:"Home Page"
+    })
+})
+
+
+const PORT  = 3000;
+app.listen(3000, ()=>{
+    console.log("Web Site is up and running");
+})
